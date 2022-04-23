8f5bb6176ba7b4a596ce9de8384d831105ce6856
Nadezhda Bilay <nadezhdabilay@gmail.com>
2022-04-23 20:03:07 +0300

    Добавление новых файлов в проект

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 var express = require('express');
-var path = require('path');
 
 var app = express();
 
-app.use(express.static(__dirname + '/js'));
-app.use(express.static(__dirname + '/css'));
-app.use(express.static(__dirname + '/images'));
+app.use(express.static(__dirname));
 
-// создаём маршрут для главной страницы
-// http://localhost:8080/
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-// запускаем сервер на порту 8080
 app.listen(3000);
-// отправляем сообщение
-console.log('started');
